72ad4ee53b5eef5ffd37a0e8dfa41c5b62b8de8e
dan sutton <danielsutton01@gmail.com>
2019-03-16 15:14:04 -0500

    Include recaptcha

--- src/asg/sections/slack_signup.cljs
+++ src/asg/sections/slack_signup.cljs
@@ -1,9 +1,11 @@
 (ns asg.sections.slack-signup
-  (:require [cljs-styled-components.reagent :refer [defstyled clj-props]]
-            [reagent.core :as r]
+  (:require [reagent.core :as r]
             [asg.styles.alignment :as align]
+            ["react-google-recaptcha" :default recaptcha]
             [asg.styles.typography :as typo]
             [clojure.string :as str]))
+
+(def client-key "6Lfx6ZcUAAAAANfkT0wc8BYKRVfXdr8vEk7fhe6w")
 
 (def email-regex (re-pattern "^[-!#$%&'*+\\/0-9=?A-Z^_a-z{|}~](\\.?[-!#$%&'*+\\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\\.?[a-zA-Z0-9])*\\.[a-zA-Z](-?[a-zA-Z0-9])+$"))
 
@@ -16,14 +18,14 @@
                      short-enough? (fn [s] (< (count s) 63))]
                  (every? short-enough? parts))))
 
-(defn input [state on-change]
+(defn input [state new-state]
   [:input {:style     {:border-color (if (or (str/blank? (:email @state))
                                              (:submittable? @state))
                                        "grey"
                                        "red")}
            :value     (:email @state)
            :type      "text"
-           :on-change #(on-change (.. % -target -value))}])
+           :on-change #(new-state :email (.. % -target -value))}])
 
 (defn submit [state]
   [:button {:disabled (not (:submittable? @state))
@@ -34,13 +36,20 @@
 
 (def state  (r/atom {:submittable? false
                      :stage        :editing
-                     :email        ""}))
+                     :email        ""
+                     :captcha-token nil}))
+
+(defn mark-valid-state [{:keys [email captcha-token] :as state}]
+  (assoc state :submittable? (and (valid-email? email)
+                                  (some? captcha-token))))
+
+(defn state->send-payload [{:keys [email captcha-token]}]
+  #js {:email email :recaptchaToken captcha-token})
 
 (defn Signup []
-  (let [change (fn [email]
-                 (swap! state merge {:email        email
-                                     :submittable? (valid-email? email)}))
-        action (fn [email]
+  (let [new-state (fn [k v]
+                    (swap! state #(mark-valid-state (assoc % k v))))
+        send! (fn [payload]
                  (swap! state assoc :stage :sending)
                  ;; fetch says this request is not OK. Not sure why
                  (-> (js/fetch signup-url
@@ -52,9 +61,9 @@
                                     ;; are set correctly
                                     ;; https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
                                     :headers #js {"Content-Type" "application/json"}
-                                    :body (.stringify js/JSON #js{:email email})})
+                                    :body (.stringify js/JSON payload)})
                      (.then (fn [response]
-                              (swap! state assoc :stage :sent)))
+                              (swap! state assoc :stage :sent :captcha-token nil)))
                      (.catch (fn [response]
                                (swap! state assoc :stage :error)))))]
     [align/Container
@@ -64,8 +73,14 @@
        :editing
        [:form {:on-submit #(do (.preventDefault %)
                                (when (:submittable? @state)
-                                 (-> @state :email action)))}
-        [input state change]
+                                 (send! (state->send-payload @state))))}
+        [:> recaptcha
+         {:sitekey client-key
+          :onChange (fn [token] (new-state :captcha-token token))
+          :style {:margin "2em"
+                  :display "flex"
+                  :justify-content "center"}}]
+        [input state new-state]
         [submit state]]
        :sending
        [:h1 "sending state"]
